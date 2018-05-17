2dabf5bcb988b72071593d02ea9f90e0c87dc362
matheusluna <matheusluna96@gmail.com>
2018-05-17 05:56:52 -0300

    modificando arquivos docker

--- kill.sh
+++ kill.sh
@@ -1,3 +1,6 @@
 docker stop app
 docker rm app
 docker rmi atividade/app
+docker stop banco
+docker rm banco
+docker rmi atividade/banco
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+docker build -t atividade/banco ./postgres
+docker run -p 5433:5432 -d --name banco atividade/banco
+
 mvn clean packege
 docker build -t atividade/app .
-docker run -p 8081:8080 -d --name app atividade/app
+docker run -p 8081:8080 -d --name app --link banco:host-banco atividade/app
